634384271daf47c37690f98979f22da7ec938f63
Aung KO <aungko.uit@gmail.com>
2022-12-28 12:45:17 +0630

    feat: Adding delete and dyanmic route

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const dotEnv = require("dotEnv");
-const items = [];
+const capitalize = require('lodash.capitalize');
+
 dotEnv.config();
 
 const app = express();
@@ -13,15 +14,25 @@ app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
-// db connection
 
+// db connection
 mongoose
   .connect("mongodb://localhost:27017/todolistDB", { useNewUrlParser: true })
   .then(() => console.log("connected"));
+
+// schema
 const itemSchema = {
   name: String,
 };
+const listSchema = {
+  name: String,
+  items: [itemSchema],
+};
+
+// model
 const Item = mongoose.model("Item", itemSchema);
+const List = mongoose.model("List", listSchema);
+
 const Item1 = new Item({ name: "Welcome to your to do list" });
 const Item2 = new Item({ name: "Press + to add new list" });
 const Item3 = new Item({ name: "check if you want to delete the list" });
@@ -46,26 +57,66 @@ app.get("/", function (req, res) {
     }
   });
 });
+// * dynamic route
+app.get("/:id", (req, res) => {
+  const id = capitalize(req.params.id);
+  console.log(req.params)
+  List.findOne({ name: id }, async (err, foundList) => {
+    if (!err) {
+      if (foundList) {
+        // console.log("something strange")
+        res.render("list", { listTitle: id, newListItems: foundList.items });
+      } else {
+      //  console.log("It worked here")
+        const list = new List({
+          name: id,
+          items: defaultItems,
+        });
+        await list.save().then(() => console.log("it saved successfully"));
+        res.redirect('/' + id);
+      }
+    }
+  });
+});
 
 app.get("/about", function (req, res) {
   res.render("about");
 });
 
-// * POST method
-app.post("/", function (req, res) {
+// * POST method (adding data)
+app.post("/",  function (req, res) {
   const itemName = req.body.newItem;
-  const list = req.body.list;
+  const listTitle = req.body.listName;
+  
   const item = new Item({ name: itemName });
-  item.save();
-  res.redirect("/");
+  if (listTitle === "Today") {
+    item.save();
+    res.redirect("/");
+  } else {
+     List.findOne({ name: listTitle }, (err, data) => {
+      if (!err) {
+        data.items.push(item);
+        data.save()
+        res.redirect('/' + listTitle)
+      }
+    });
+  }
 });
 
 // * DELETE method
 app.post("/delete", function (req, res) {
   const id = req.body.check;
-  Item.findByIdAndRemove(id, (err) => console.log("removed successfully"));
-  res.redirect('/')
-}
-);
+  const listTitle = req.body.listTitle;
+  if (listTitle === "Today")
+  {Item.findByIdAndRemove(id, (err) => console.log("removed successfully"));
+  res.redirect("/");}
+  else {
+    List.findOneAndUpdate({name: listTitle}, {$pull: {items: {_id: id} }}, function(err, foundList) {
+      if (!err) {
+        res.redirect('/' + listTitle)
+      }
+    })
+  }
+});
 
 module.exports = app;
